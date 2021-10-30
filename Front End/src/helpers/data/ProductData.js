3a19b71f37f30168af0eb9a7db9a1d29655c48ab
Chris Meffley <cmeffley@gmail.com>
2021-10-30 13:01:31 -0400

    working on env localhost bug

--- Front End/src/helpers/data/ProductData.js
+++ Front End/src/helpers/data/ProductData.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
-// import config from '../apiKeys';
+import config from '../apiKeys';
 
-// const dbUrl = config.databaseURL;
+const dbUrl = config.databaseURL;
 
 const getAllProducts = () => new Promise((resolve, reject) => {
-  axios.get('http://localhost:28713/api/products')
+  axios.get(`${dbUrl}/api/products`)
     .then((response) => resolve(response.data))
     .catch((error) => reject(error));
 });
