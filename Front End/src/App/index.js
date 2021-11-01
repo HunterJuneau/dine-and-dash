c643d97900ad9fdfdd83d6cc650469d57467426f
Chris Meffley <cmeffley@gmail.com>
2021-11-01 19:41:52 -0400

    basic set up for sign in and out

--- Front End/src/App/index.js
+++ Front End/src/App/index.js
@@ -1,12 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import firebase from 'firebase';
 import './App.scss';
 import NavBar from '../components/NavBar';
 import Routes from '../helpers/Routes';
 import getAllProducts from '../helpers/data/ProductData';
 
 function App() {
+  const [user, setUser] = useState(null);
   const [products, setProducts] = useState([]);
+
+  firebase.auth().onAuthStateChanged((authed) => {
+    if (authed) {
+      const userInfo = {
+        userName: authed.displayName,
+        uid: authed.uid
+      };
+      setUser(userInfo);
+    } else if (user || user === null) {
+      setUser(false);
+    }
+  }, []);
 
   useEffect(() => getAllProducts().then(setProducts), []);
   return (
