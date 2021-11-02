be7ec7523beae569780a8da90bc1df170353da1f
Chris Meffley <cmeffley@gmail.com>
2021-11-01 21:36:42 -0400

    able to view on product by itself

--- Front End/src/components/products/Productcard.js
+++ Front End/src/components/products/Productcard.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import {
   Card,
   CardText,
   // CardImg,
   CardBody,
   CardTitle,
-  // Button
+  Button
 } from 'reactstrap';
 
 function Productcard({ ...productInfo }) {
+  const history = useHistory();
+
   return (
     <div>
       <Card>
-        <CardTitle tag='h5'>{productInfo.productName}</CardTitle>
+        <CardTitle tag='h3'>{productInfo.productName}</CardTitle>
           <CardBody>
-            {productInfo.type}
+            <CardTitle tag='h6'>{productInfo.type}</CardTitle>
             <CardText>{productInfo.productDescription}</CardText>
             <CardText>Price: ${productInfo.price}</CardText>
             <CardText>Quantity: {productInfo.quantity}</CardText>
-            <CardText>{(productInfo.forSale === true) ? "For Sale" : "For Rent Only"}</CardText>
+            <CardText>{productInfo.forSale ? 'For Sale or Rent' : 'For Rent Only'}</CardText>
           </CardBody>
+          <Button onClick={() => history.push(`/products/${productInfo.id}`)}>See Details</Button>
       </Card>
     </div>
   );
