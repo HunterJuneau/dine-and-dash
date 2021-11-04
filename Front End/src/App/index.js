cb076604e0a4dbcc81854868c250b2a39c88317a
BenJarrett <b.cl.jarrett@gmail.com>
2021-11-04 18:08:49 -0500

    fixed

--- Front End/src/App/index.js
+++ Front End/src/App/index.js
@@ -4,18 +4,18 @@ import './App.scss';
 import NavBar from '../components/NavBar';
 import Routes from '../helpers/Routes';
 import { getAllProducts } from '../helpers/data/ProductData';
-import { getAllUsers, getUserOrders } from '../helpers/data/UserData';
+import { getAllUsers } from '../helpers/data/UserData';
 
 function App() {
   const [products, setProducts] = useState([]);
   const [users, setUsers] = useState([]);
-  const [userOrders, setUserOrders] = useState([]);
+  // const [userOrders, setUserOrders] = useState([]);
 
   useEffect(() => getAllProducts().then(setProducts), []);
   useEffect(() => getAllUsers().then(setUsers), []);
-  useEffect(() => getUserOrders().then(setUserOrders), []);
+  // useEffect(() => getUserOrders().then(setUserOrders), []);
 
-  console.warn(userOrders);
+  // console.warn(userOrders);
   return (
     <>
       <Router>
